e4b4890a14fafefab2d2cde0d9702f795567a2e7
NotZack <zackarynichol@gmail.com>
2018-12-29 13:51:56 -0600

    Added economy file and fixed gitignore

---
+++ src/main/java/societalProductivity/Economy.java
@@ -0,0 +1,34 @@
+package societalProductivity;
+
+import cowParts.Cognition;
+import cowParts.Cow;
+import metaEnvironment.EventLogger;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Handles every money/trade transaction.
+ */
+public class Economy {
+
+    /**
+     * Gives a given cow a given amount of money to add to their finance cognition.
+     * @param cowToGiveMoney The cow to give money to
+     * @param amountToGive The amount of money to give the cow
+     */
+    public static void giveMoney(@NotNull Cow cowToGiveMoney, int amountToGive) {
+        Cognition cowFinances = cowToGiveMoney.self;
+
+        cowFinances.setIncome(amountToGive);
+        EventLogger.createLoggedEvent(cowToGiveMoney, "New Income", 0, "income", amountToGive);
+
+        if (cowFinances.getDebt() == 0) {
+            cowFinances.setSavings(cowFinances.getSavings() + amountToGive);
+            EventLogger.createLoggedEvent(cowToGiveMoney, "Payment", 0, "savings", amountToGive);
+        }
+
+        else {
+            cowFinances.setDebt(-amountToGive);
+            EventLogger.createLoggedEvent(cowToGiveMoney, "Payment", 0, "debt", -amountToGive);
+        }
+    }
+}
